9fcb01e26779eb10c833f7ae36f6dea9a617fadb
Administrator <Administrator@WIN-GIH3NH05LU4>
2024-12-11 01:45:21 -0500

    节点总算力更新

--- src/views/ability/ability.vue
+++ src/views/ability/ability.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
     <nav-header title="算力中心" @clickRight="pushPath('/nodeRecord')">
-      <span slot="headerRight" class="right_img"
-        ><img src="@img/home/icon_record.png" alt=""
-      /></span>
+      <span slot="headerRight" class="right_img"><img src="@img/home/icon_record.png" alt="" /></span>
     </nav-header>
     <div class="main_content">
       <div class="lock_top">
         <p>已释放算力</p>
-        <h2 >
+        <h2>
           {{ nodeInfo.releaseAbility || "0.00"
-          }}<span style="color: green; font-size: 14px; margin-left: 5px" @click="withDrawAbility"
-            >兑现</span
-          >
-          <i class="iconfont icon-arrow-right main_color"  @click="pushPath('/withdrawRecord')" style="float: right;"/>
+          }}<span style="color: green; font-size: 14px; margin-left: 5px" @click="withDrawAbility">兑现</span>
+          <i class="iconfont icon-arrow-right main_color" @click="pushPath('/withdrawRecord')" style="float: right;" />
         </h2>
         <ol class="flex">
           <li>
             <h4>
-              {{ nodeInfo.nodeAbility + nodeInfo.shareAbility || "0.00" }}
+              {{ nodeInfo.nodeAbility + nodeInfo.shareAbility + nodeInfo.freeAbility || "0.00" }}
             </h4>
             <p>总算力</p>
           </li>
@@ -32,31 +28,20 @@
           </li>
         </ol>
       </div>
-      <ol
-        v-for="item in nodes"
-        :key="item.id"
-        class="lock_box"
-        @click="goConfirm(item)"
-      >
+      <ol v-for="item in nodes" :key="item.id" class="lock_box" @click="goConfirm(item)">
         <li class="flex_between">
           <h3 class="flex_center">
-            <van-image
-              width="80px"
-              height="80px"
-              :src="`${baseImgUrl}/${item.id}`"
-              fit="cover"
-              alt="Example Image"
-              style="margin-right: 6px"
-            />{{ item.name || "--" }}
+            <van-image width="80px" height="80px" :src="`${baseImgUrl}/${item.id}`" fit="cover" alt="Example Image"
+              style="margin-right: 6px" />{{ item.name || "--" }}
           </h3>
           <i class="iconfont icon-arrow-right main_color" />
         </li>
         <li class="flex">
           <div>
-            <label class="flex main_color"
-              ><p>{{ item.gas }}</p>
-              <em>%</em></label
-            >
+            <label class="flex main_color">
+              <p>{{ item.gas }}</p>
+              <em>%</em>
+            </label>
             <span>每日释放比例</span>
           </div>
           <div>
@@ -109,7 +94,7 @@ export default {
     isDisable(data) {
       return data.state === "disable";
     },
-    withDrawAbility(){
+    withDrawAbility() {
       this.pushPath('/withdrawAbility')
     },
     goConfirm(data) {
@@ -128,59 +113,78 @@ export default {
 .right_img {
   width: 20px;
 }
+
 .lock_top {
   padding: 10px 15px;
+
   p {
     font-size: 12px;
     color: $subFontColor;
   }
+
   h2 {
     margin-top: 10px;
     font-size: 24px;
     line-height: 28px;
   }
+
   ol {
     margin-top: 15px;
+
     li {
       flex: 1;
+
       h4 {
         line-height: 26px;
         margin-bottom: 5px;
       }
+
       &:last-child {
         max-width: 25%;
       }
     }
   }
 }
+
 .disabled {
-  pointer-events: none; /* 禁用点击事件 */
-  opacity: 0.6; /* 设置半透明效果 */
-  cursor: not-allowed; /* 显示禁用光标 */
-  filter: grayscale(100%); /* 灰化处理（可选） */
+  pointer-events: none;
+  /* 禁用点击事件 */
+  opacity: 0.6;
+  /* 设置半透明效果 */
+  cursor: not-allowed;
+  /* 显示禁用光标 */
+  filter: grayscale(100%);
+  /* 灰化处理（可选） */
 }
+
 .lock_box {
   margin: 5px 15px 15px;
   background: $blockColor;
   padding: 0 12px;
   border-radius: 4px;
+
   li {
     &:first-child {
       padding: 12px 0 10px;
       border-bottom: 1px solid rgba($lineColor, 0.5);
+
       i {
         font-size: 14px;
       }
     }
+
     &:last-child {
       padding: 12px 0;
+
       div {
         flex: 1;
         line-height: 20px;
+
         p {
           font-size: 16px;
           font-weight: 600;
         }
+
         span {
           display: block;
           color: $subFontColor;
