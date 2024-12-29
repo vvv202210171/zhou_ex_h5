e0136da49535b80638ed04b545733612d8037610
gsfzynpvv <vvv@qq.com>
2024-12-29 21:26:39 +0800

    修改资产页面

--- src/views/wallet/balanceRecord.vue
+++ src/views/wallet/balanceRecord.vue
@@ -5,30 +5,32 @@
       <div class="record_top_box">
         <div class="balance_box">
           <span>{{ $t('tradeLang.lang49') }}</span>
-          <h2>{{ (Number(coinBalance.usable) + Number(coinBalance.disable))|mathFloor(8) }}</h2>
+          <h2>{{ (Number(coinBalance.usable) + Number(coinBalance.disable)) | mathFloor(8) }}</h2>
           <div class="flex_between">
             <p>{{ $t('tradeLang.lang42') }}</p>
-            <span>{{ coinBalance.usable|mathFloor(8) }}</span>
+            <span>{{ coinBalance.usable | mathFloor(8) }}</span>
           </div>
           <div class="flex_between">
             <p>{{ $t('tradeLang.lang43') }}</p>
-            <span>{{ coinBalance.disable|mathFloor(8) }}</span>
+            <span>{{ coinBalance.disable | mathFloor(8) }}</span>
           </div>
           <div class="flex_between">
             <p>{{ $t('tradeLang.lang44') }}({{ rateData.coin }})</p>
-            <span>{{ ((Number(coinBalance.usable) + Number(coinBalance.disable)) * coinBalance.usdt *rateData.price)|mathFloor(2) }}</span>
+            <span>{{ ((Number(coinBalance.usable) + Number(coinBalance.disable)) * coinBalance.usdt
+              * rateData.price) |mathFloor(2) }}</span>
           </div>
         </div>
       </div>
-      <van-tabs v-model="active" border :sticky="isSticky" :offset-top="headerHeight" swipeable line-width="20" line-height="2">
+      <van-tabs v-model="active" border :sticky="isSticky" :offset-top="headerHeight" swipeable line-width="20"
+        line-height="2">
         <van-tab :title="$t('tradeLang.lang50')">
-          <billList :key="coinName+'in'" type="in" :coin-name="coinName" />
+          <billList :key="coinName + 'in'" type="in" :coin-name="coinName" />
         </van-tab>
         <van-tab :title="$t('tradeLang.lang51')">
-          <billList :key="coinName+'out'" type="out" :coin-name="coinName" />
+          <billList :key="coinName + 'out'" type="out" :coin-name="coinName" />
         </van-tab>
         <van-tab :title="$t('tradeLang.lang52')">
-          <billList :key="coinName+'system'" type="system" :coin-name="coinName" />
+          <billList :key="coinName + 'system'" type="system" :coin-name="coinName" />
         </van-tab>
       </van-tabs>
     </div>
@@ -78,31 +80,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.record_top_box{
+.record_top_box {
   background-color: $bgColor;
   padding: 8px 15px;
 }
-.balance_box{
+
+.balance_box {
   background-color: $mainColor;
   padding: 15px;
   color: $bgColor;
   border-radius: 4px;
-  h2{
+
+  h2 {
     line-height: 30px;
     font-size: 26px;
     margin: 10px 0;
   }
-  span{
+
+  span {
     font-size: 12px;
   }
-  div{
+
+  div {
     margin-top: 5px;
     line-height: 20px;
     font-size: 13px;
-    p{
+
+    p {
       font-size: 12px;
     }
   }
 }
-
 </style>
