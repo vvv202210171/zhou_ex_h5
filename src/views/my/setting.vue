122182663199c88d115e492ec60c7dc6181ebd86
gsfzynpvv <vvv@qq.com>
2025-01-09 23:28:41 +0800

    locale

--- src/views/my/setting.vue
+++ src/views/my/setting.vue
@@ -4,19 +4,19 @@
     <nav-header :title="$t('accountLang.lang144')" />
     <div class="main_content main_bg">
       <ol class="menu_box">
-        <li class="flex" @click="langSheet=true">
-          <p>{{$t('accountLang.lang145')}}</p>
-          <em v-if="$i18n.locale=='CN'">繁體中文</em>
-          <em v-if="$i18n.locale=='EN'">English</em>
-          <em v-if="$i18n.locale=='KO'">한국어</em>
-          <em v-if="$i18n.locale=='JP'">日本語</em>
-          <em v-if="$i18n.locale=='DE'">Deutsch</em>
-          <em v-if="$i18n.locale=='ES'">España</em>
-          <em v-if="$i18n.locale=='FR'">Français</em>
-          <em v-if="$i18n.locale=='IT'">Italia</em>
-          <em v-if="$i18n.locale=='PT'">Portugal</em>
-          <em v-if="$i18n.locale=='ID'">Indonesia</em>
-          <em v-if="$i18n.locale=='VI'">TiếngViệt</em>
+        <li class="flex" @click="langSheet = true">
+          <p>{{ $t('accountLang.lang145') }}</p>
+          <em v-if="$i18n.locale == 'CN'">繁體中文</em>
+          <em v-if="$i18n.locale == 'EN'">English</em>
+          <em v-if="$i18n.locale == 'KO'">한국어</em>
+          <em v-if="$i18n.locale == 'JP'">日本語</em>
+          <em v-if="$i18n.locale == 'DE'">Deutsch</em>
+          <em v-if="$i18n.locale == 'ES'">España</em>
+          <em v-if="$i18n.locale == 'FR'">Français</em>
+          <em v-if="$i18n.locale == 'IT'">Italia</em>
+          <em v-if="$i18n.locale == 'PT'">Portugal</em>
+          <em v-if="$i18n.locale == 'ID'">Indonesia</em>
+          <em v-if="$i18n.locale == 'VI'">TiếngViệt</em>
 
           <i class="iconfont icon-arrow-right"></i>
         </li>
@@ -26,47 +26,50 @@
           <i class="iconfont icon-arrow-right"></i>
         </li> -->
         <li class="flex" @click="showUserAgreePopup()">
-          <p>{{$t('accountLang.lang25')}}</p>
+          <p>{{ $t('accountLang.lang25') }}</p>
           <i class="iconfont icon-arrow-right"></i>
         </li>
         <li class="flex" @click="showPrivacyPolicyPopup()">
-          <p>{{$t('accountLang.lang26')}}</p>
+          <p>{{ $t('accountLang.lang26') }}</p>
           <i class="iconfont icon-arrow-right"></i>
         </li>
         <li class="flex" v-show="!isApp" @click="pushPath('/downloadApp')">
-          <p>{{$t('accountLang.lang156')}}</p>
+          <p>{{ $t('accountLang.lang156') }}</p>
           <i class="iconfont icon-arrow-right"></i>
         </li>
         <li class="flex">
-          <p>{{$t('accountLang.lang146')}}</p>
-          <span>V1.{{appVersion}}</span>
+          <p>{{ $t('accountLang.lang146') }}</p>
+          <span>V1.{{ appVersion }}</span>
         </li>
       </ol>
 
       <div class="exit_box" v-if="isLogin">
-        <van-button type="primary" size="large" @click="exitPopup = true">{{$t('accountLang.lang147')}}</van-button>
+        <van-button type="primary" size="large" @click="exitPopup = true">{{ $t('accountLang.lang147') }}</van-button>
       </div>
     </div>
-    <van-action-sheet v-model="langSheet" :actions="langList" @select="onSelectLang" :cancel-text="$t('commonLang.lang1')" />
+    <van-action-sheet v-model="langSheet" :actions="langList" @select="onSelectLang"
+      :cancel-text="$t('commonLang.lang1')" />
     <van-popup v-model="exitPopup" position="bottom" close-on-popstate class="model_popup">
-      <div class="model_popup_top"><i @click="exitPopup=false"></i></div>
+      <div class="model_popup_top"><i @click="exitPopup = false"></i></div>
       <div class="exit_popup_box">
-        <h3>{{$t('accountLang.lang147')}}</h3>
-        <p>{{$t('accountLang.lang148')}}</p>
+        <h3>{{ $t('accountLang.lang147') }}</h3>
+        <p>{{ $t('accountLang.lang148') }}</p>
         <div class="model_popup_btn flex">
-          <van-button @click="exitPopup=false">{{$t('commonLang.lang1')}}</van-button>
-          <van-button @click="exit()">{{$t('commonLang.lang3')}}</van-button>
+          <van-button @click="exitPopup = false">{{ $t('commonLang.lang1') }}</van-button>
+          <van-button @click="exit()">{{ $t('commonLang.lang3') }}</van-button>
         </div>
       </div>
     </van-popup>
 
-    <van-popup class="right_popup" v-model="userAgreementPopup" position="right" close-on-popstate overlay-class="block_bg">
+    <van-popup class="right_popup" v-model="userAgreementPopup" position="right" close-on-popstate
+      overlay-class="block_bg">
       <nav-header :title="$t('accountLang.lang25')" />
       <div class="main_content">
         <userAgreement />
       </div>
     </van-popup>
-    <van-popup class="right_popup" v-model="privacyPolicyPopup" position="right" close-on-popstate overlay-class="block_bg">
+    <van-popup class="right_popup" v-model="privacyPolicyPopup" position="right" close-on-popstate
+      overlay-class="block_bg">
       <nav-header :title="$t('accountLang.lang26')" />
       <div class="main_content">
         <privacyPolicy />
@@ -124,9 +127,14 @@ export default {
         this.langSheet = false;
         return
       }
+      this.$i18n.locale = data.type;
       localStorage.setItem('locale',data.type);
-      window.location.reload();
-      this.langSheet = false;
+
+   // 延迟刷新页面，确保切换生效
+      setTimeout(() => {
+        window.location.reload();
+      }, 100); // 延迟刷新
+          this.langSheet = false;
     },
     exit(){
       logout().then(res=>{
@@ -151,31 +159,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.menu_box{
+.menu_box {
   margin-top: 8px;
   padding-left: 15px;
   background-color: var(--inputBox-bgColor);
-  li{
+
+  li {
     padding: 12px 15px 12px 0;
     border-bottom: 1px solid $lineColor;
-    &:last-child{
+
+    &:last-child {
       border: none;
     }
-    p{
+
+    p {
       flex: 1;
       line-height: 20px;
     }
-    i{
-      flex:1;
+
+    i {
+      flex: 1;
       max-width: 18px;
       min-width: 18px;
       line-height: 21px;
       height: 20px;
       font-size: 14px;
       text-align: right;
-      color: rgba($subFontColor,.5);
+      color: rgba($subFontColor, .5);
     }
-    span{
+
+    span {
       font-size: 13px;
       color: $subFontColor;
       line-height: 20px;
@@ -183,17 +196,19 @@ export default {
   }
 }
 
-.exit_box{
+.exit_box {
   position: absolute;
   width: 100%;
   bottom: 0;
   left: 0;
   padding: 20px 15px;
 }
-.exit_popup_box{
+
+.exit_popup_box {
   padding: 15px 20px;
   text-align: center;
-  p{
+
+  p {
     font-size: 12px;
     color: $subFontColor;
     margin: 10px 0 30px;
